e9abacda9b2bf0ccb9b405f29126dba99043f744
yoshikawa.kohei <yoshikawa.kohei@classmethod.jp>
2024-07-21 04:30:56 +0000

    add terraform workflow

--- terraform/environments/common/backend.tf
+++ terraform/environments/common/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    key                        = "common/handson_ecs/tfstate"
-    region                     = "ap-northeast-1"
-    terraform_state_lock_table = "tmp-hands-on-tf-state-lock"
+    key    = "common/handson_ecs/tfstate"
+    region = "ap-northeast-1"
+    bucket = "delete-test-kohei"
   }
 }
